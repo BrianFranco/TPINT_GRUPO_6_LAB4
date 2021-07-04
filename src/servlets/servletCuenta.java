9f1f432d7f12e0d0ab86b0cdea94610580306a75
Lorgum <33044482+Lorgum@users.noreply.github.com>
2021-07-04 14:26:08 -0300

    Create servletCuenta.java

---
+++ src/servlets/servletCuenta.java
@@ -0,0 +1,61 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entidad.Cuenta;
+import negocio.cuentaNeg;
+import negocioImpl.cuentaNegImpl;
+import presentacion.controller.RequestDispatcher;
+
+import java.text.SimpleDateFormat;  
+import java.util.Date;
+
+
+/**
+ * Servlet implementation class servletCuenta
+ */
+@WebServlet("/servletCuenta")
+public class servletCuenta extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	cuentaNeg negCuenta = new cuentaNegImpl();   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servletCuenta() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("btnGenerar")!=null) {
+			Cuenta x = new Cuenta();
+			x.setCBU(request.getParameter("nroCBU"));
+			x.setTipoCuenta(request.getParameter("comboCuenta"));
+			x.setActivo(true);
+			x.setSaldo(request.getParameter("saldo"));
+			x.setFecha(request.getParameter("fecha"));
+			
+			negCuenta.insertar(x);
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/ABMLCuentas.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
